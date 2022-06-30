ae031b4242e9c150d94db6ee013c3440a6adc7a8
Leonardo Moreira <lmoreira@monorail.com>
2022-06-30 17:51:20 -0300

    packages update and google tag manager

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,33 +1,64 @@
-import Document, { Html, Head, Main, NextScript, DocumentContext } from "next/document";
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+} from "next/document";
+// eslint-disable-next-line @next/next/no-script-in-document
+import Script from "next/script";
 import { GA_TRACKING_ID } from "../utils/gtag";
 
 export default class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
-      const initialProps = await Document.getInitialProps(ctx)
-      return {...initialProps}
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
   }
-  render () {
+  render() {
     return (
       <Html>
         <Head>
+        <Script
+            id="google-tag-manager"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-PWXV2MQ');`,
+            }}
+          ></Script>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link href="https://fonts.googleapis.com/css2?family=Righteous&display=swap" rel="stylesheet" />
-          <link href="https://fonts.googleapis.com/css2?family=Mada:wght@400;600&display=swap" rel="stylesheet"></link>
-          <meta name="title" content="Rafael Gerônimo"/>
-          <meta name="description" content="Desenvolvedor web em um foguete rumo à grandes conquistas."/>
-          <meta name="keywords" content="rafael geronimo, desenvolvedor, programador, web development, sistemas, dev"/>
-          <meta name="robots" content="index, follow"/>
-          <meta httpEquiv="Content-Type" content="text/html; charset=utf-8"/>
-          <meta name="language" content="Portuguese"/>
-          <meta name="revisit-after" content="2 days"/>
-          <meta name="author" content="Rafael Gerônimo"/>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Righteous&display=swap"
+            rel="stylesheet"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Mada:wght@400;600&display=swap"
+            rel="stylesheet"
+          ></link>
+          <meta name="title" content="Rafael Gerônimo" />
+          <meta
+            name="description"
+            content="Desenvolvedor web em um foguete rumo à grandes conquistas."
+          />
+          <meta
+            name="keywords"
+            content="rafael geronimo, desenvolvedor, programador, web development, sistemas, dev"
+          />
+          <meta name="robots" content="index, follow" />
+          <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+          <meta name="language" content="Portuguese" />
+          <meta name="revisit-after" content="2 days" />
+          <meta name="author" content="Rafael Gerônimo" />
           {/* <title>Rafael Gerônimo</title> */}
           {/* Global Site Tag (gtag.js) - Google Analytics */}
           <script
             async
             src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
           />
-          <script
+          {/* <script
             dangerouslySetInnerHTML={{
               __html: `
               window.dataLayer = window.dataLayer || [];
@@ -36,15 +67,23 @@ export default class MyDocument extends Document {
               gtag('config', '${GA_TRACKING_ID}', {
                 page_path: window.location.pathname,
               });
-          `
+          `,
             }}
-          />
+          /> */}
         </Head>
         <body>
+          <noscript>
+            <iframe
+              src="https://www.googletagmanager.com/ns.html?id=GTM-PWXV2MQ"
+              height="0"
+              width="0"
+              // style="display:none;visibility:hidden"
+            ></iframe>
+          </noscript>
           <Main />
           <NextScript />
         </body>
       </Html>
-    )
+    );
   }
 }
