06905014d3b2e28d32f14abadfc91803e3a8ecb4
Rafael Geronimo <rafael@geronimo.dev>
2022-07-26 22:48:33 -0300

    about animate

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -3,18 +3,19 @@ import Image from "next/image";
 import styles from "../../styles/about.module.scss";
 import profilePic from "../../img/profile.png";
 import Link from "next/link";
+import 'animate.css';
 
 export default function About() {
   return (
     <main className={styles.mainContainer}>
       <div>
-        <h1>Quem sou eu</h1>
-        <h2>
+        <h1 className={`animate__animated animate__slideInLeft`}>Quem sou eu</h1>
+        <h2 className={`animate__animated animate__zoomIn`}>
           Olá!
           <br /> Me chamo <strong>Rafael Gerônimo</strong>,<br /> e sou um
           desenvolvedor de softwares.
         </h2>
-        <p>
+        <p className={`animate__animated animate__bounceIn`}>
           Com mais de 15 anos de experiência profissional na área de tecnologia,
           trabalhando com hardware e redes, decidi realizar uma transação de
           carreira, migrando para a área de desenvolvimento de softwares.
@@ -27,11 +28,11 @@ export default function About() {
           conhecimentos nas Hard Skills e também desenvolver as habilidades de
           Soft Skills, concluindo em fevereiro de 2022.
         </p>
-        <p>
+        <p className={`animate__animated animate__bounceIn`}>
           Durante os estudos, foram desenvolvidos mais de 30 projetos pela Trybe
           e outros tantos na Unicesumar.
         </p>
-        <p>
+        <p className={`animate__animated animate__bounceIn`}>
           Em abril de 2022, fui descoberto pela{" "}
           <Link href={`https://wolven.com.br`}>
             <a>Wolven</a>
